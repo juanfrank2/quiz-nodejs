b1da3db1e51061ee12bcf7a0dec1e0b19359b587
Juan Fran <juanfrank2@gmail.com>
2015-07-25 14:06:34 +0200

    Cambiado uso find por findById

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -2,14 +2,14 @@
 var models = require('../models/models.js');
 
 exports.load = function (req,res, next, quizId) {
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 		function(quiz){
 			if (quiz){
 				req.quiz = quiz;
 				next();
 			}
 			else {
-				next (new Error ('No existe quizId='+quizId)
+				next (new Error ('No existe quizId='+quizId));
 			}
 		}
 	).catch(function(error) {next(error);});
@@ -29,7 +29,7 @@ exports.show = function(req, res) {
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	var resultado 'Incorrecto';
+	var resultado = 'Incorrecto';
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
