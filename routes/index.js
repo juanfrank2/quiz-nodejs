44204d8a65f41a4429633fea3763886daa5b5e2a
Juan Fran <juanfrank2@gmail.com>
2015-07-25 14:02:24 +0200

    Se añade autoload para quizId

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload con :quizId
+router.param('quizId', quizController.load);
+
 // Listado de preguntas
 router.get('/quizes',	quizController.index);
 
